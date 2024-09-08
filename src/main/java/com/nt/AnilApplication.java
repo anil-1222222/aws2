a7d7cd97e54066a5a57a8624dbe99cb8ac95196f
Anil <anil@123>
2024-09-08 17:16:43 +0530

    Spring

---
+++ src/main/java/com/nt/AnilApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnilApplication.class, args);
+	}
+
+}
